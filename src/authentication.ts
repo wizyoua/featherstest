a90c56a39ec2d53e8bb675fd199ebbf1fd7ec508
Gianni <gianni@Admins-MacBook-Pro-2.local>
2024-09-17 14:47:50 -0700

    final changes

--- src/authentication.ts
+++ src/authentication.ts
@@ -1,7 +1,9 @@
 // For more information about this file see https://dove.feathersjs.com/guides/cli/authentication.html
+import type { Params } from '@feathersjs/feathers'
 import { AuthenticationService, JWTStrategy } from '@feathersjs/authentication'
 import { LocalStrategy } from '@feathersjs/authentication-local'
 import { oauth, OAuthStrategy } from '@feathersjs/authentication-oauth'
+import type { OAuthProfile } from '@feathersjs/authentication-oauth'
 
 import type { Application } from './declarations'
 
@@ -11,12 +13,26 @@ declare module './declarations' {
   }
 }
 
+class GitHubStrategy extends OAuthStrategy {
+  async getEntityData(profile: OAuthProfile, existing: any, params: Params) {
+    const baseData = await super.getEntityData(profile, existing, params)
+
+    return {
+      ...baseData,
+      // The GitHub profile image
+      avatar: profile.avatar_url,
+      // The user email address (if available)
+      email: profile.email
+    }
+  }
+}
+
 export const authentication = (app: Application) => {
   const authentication = new AuthenticationService(app)
 
   authentication.register('jwt', new JWTStrategy())
   authentication.register('local', new LocalStrategy())
-  authentication.register('github', new OAuthStrategy())
+  authentication.register('github', new GitHubStrategy())
 
   app.use('authentication', authentication)
   app.configure(oauth())
